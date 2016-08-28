56fedf1ed417c01fec2d8254a2a0434e4f142d85
Konstantin Tokarev <annulen@yandex.ru>
2016-08-28 23:55:03 +0300

    Build TestWebKit2.
    Change-Id: I41fabe1a6fc12ab6109f2c82a98b2eb568389a0b

--- Tools/TestWebKitAPI/PlatformQt.cmake
+++ Tools/TestWebKitAPI/PlatformQt.cmake
@@ -46,6 +46,11 @@ set(test_main_SOURCES
     qt/main.cpp
 )
 
+set(webkit2_api_harness_SOURCES
+    qt/PlatformUtilitiesQt.cpp
+    qt/PlatformWebViewQt.cpp
+)
+
 set(bundle_harness_SOURCES
     qt/InjectedBundleControllerQt.cpp
     qt/PlatformUtilitiesQt.cpp
@@ -78,3 +83,61 @@ endif ()
 add_test(TestWebCore ${TESTWEBKITAPI_RUNTIME_OUTPUT_DIRECTORY}/WebCore/TestWebCore)
 set_tests_properties(TestWebCore PROPERTIES TIMEOUT 60)
 set_target_properties(TestWebCore PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${TESTWEBKITAPI_RUNTIME_OUTPUT_DIRECTORY}/WebCore)
+
+if (ENABLE_WEBKIT2)
+    add_executable(TestWebKit2
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/AboutBlankLoad.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/CanHandleRequest.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/CookieManager.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/DocumentStartUserScriptAlertCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/DOMWindowExtensionBasic.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/DOMWindowExtensionNoCache.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/DownloadDecideDestinationCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/EvaluateJavaScript.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/FailedLoad.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/Find.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/ForceRepaint.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/FrameMIMETypeHTML.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/FrameMIMETypePNG.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/Geolocation.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/GetInjectedBundleInitializationUserDataCallback.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/HitTestResultNodeHandle.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/InjectedBundleBasic.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/InjectedBundleFrameHitTest.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/InjectedBundleInitializationUserDataCallbackWins.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/LoadAlternateHTMLStringWithNonDirectoryURL.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/LoadCanceledNoServerRedirectCallback.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/LoadPageOnCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/MouseMoveAfterCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/NewFirstVisuallyNonEmptyLayout.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/NewFirstVisuallyNonEmptyLayoutFails.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/NewFirstVisuallyNonEmptyLayoutForImages.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/NewFirstVisuallyNonEmptyLayoutFrames.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/PageLoadBasic.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/PageLoadDidChangeLocationWithinPageForFrame.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/ParentFrame.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/PendingAPIRequestURL.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/PreventEmptyUserAgent.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/PrivateBrowsingPushStateNoHistoryCallback.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/ReloadPageAfterCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/ResizeWindowAfterCrash.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/RestoreSessionStateContainingFormData.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/ShouldGoToBackForwardListItem.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/TextFieldDidBeginAndEndEditing.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/UserMedia.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/UserMessage.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WillSendSubmitEvent.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKPageCopySessionStateWithFiltering.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKPageGetScaleFactorNotZero.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKPreferences.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKRetainPtr.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKString.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKStringJSString.cpp
+    ${TESTWEBKITAPI_DIR}/Tests/WebKit2/WKURL.cpp
+    )
+
+    target_link_libraries(TestWebKit2 ${test_webkit2_api_LIBRARIES})
+    add_test(TestWebKit2 ${TESTWEBKITAPI_RUNTIME_OUTPUT_DIRECTORY}/WebKit2/TestWebKit2)
+    set_tests_properties(TestWebKit2 PROPERTIES TIMEOUT 60)
+    set_target_properties(TestWebKit2 PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${TESTWEBKITAPI_RUNTIME_OUTPUT_DIRECTORY}/WebKit2)
+endif ()
